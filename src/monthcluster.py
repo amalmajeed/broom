a48500bac2b74e32cf49653386c97514724dfad1
Amal Majeed <amalmajeed7@gmail.com>
2022-06-25 09:35:37 -0600

    Added monthcluster module

---
+++ src/monthcluster.py
@@ -0,0 +1,42 @@
+'''
+
+    Author: Amal Majeed
+    Version: 1.0
+    Created: 06/25/22
+    Updated: 06/25/22
+    Purpose: Script that automatically clusters up folders and files that are older than 6 months, 3 months and 1 months 
+            for macOS environment.
+
+'''
+
+from calendar import month
+import os
+import time
+import shutil
+
+oneMonth = float(2630000)
+
+
+class MonthCluster:
+    def __init__(self, month=6, user=os.getlogin()):
+        self.month = month
+        self.threshold = self.month * oneMonth
+        self.user = user
+
+    def folderiser(self, endpoint="Downloads"):
+        self.root = "/Users"+"/"+self.user+"/"+endpoint
+        self.destination = self.root+"/"+"Older_"+str(self.month)+"Month"
+        if(os.path.exists(self.destination)):
+            print(f"Target path exists for {self.month} month folder!")
+        else:
+            os.makedirs(self.destination)
+        self.listFiles = os.listdir(self.root)
+        for file in self.listFiles:
+            tMod = os.path.getmtime(self.root+"/"+file)
+            gap = int((time.time()-tMod)//oneMonth)
+            if(gap >= self.month):
+                try:
+                    shutil.move(self.root+'/'+file, self.destination)
+                except Exception as e:
+                    print(
+                        f"Exception occured while moving to {self.destination} as : {e}\n")
